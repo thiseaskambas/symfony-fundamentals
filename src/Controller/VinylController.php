25fedd4647c6edb8748d0d0f73df9edd7f1d1457
thiseaskambas <tkambas@yahoo.com>
2023-03-27 11:15:50 -0400

    using knpTimeBundle service on the backend

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\TimeBundle\DateTimeFormatter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,10 +29,14 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
+    public function browse(DateTimeFormatter $timeFormatter, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
         $mixes = $this->getMixes();
+
+        foreach ($mixes as $key => $mix){
+            $mixes[$key]['ago'] = $timeFormatter->formatDiff($mix['createdAt']);
+        }
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
