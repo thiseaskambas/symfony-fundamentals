a5969f491a32f2c05b6c11882eb1475a69686ba7
thiseaskambas <tkambas@yahoo.com>
2023-03-28 15:27:48 -0400

    setting local environment variables

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,6 +3,8 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -12,17 +14,27 @@ class MixRepository
 
     //dependency injection :
     public function __construct(
-        private HttpClientInterface $httpClient,
+        private HttpClientInterface $githubContentClient,
         private CacheInterface $cache,
         #[Autowire('%kernel.debug%')]
-        private bool $isDebug //in services.yaml we bind the 'kernel.debug' parameter value to it
+        private bool $isDebug, //in services.yaml we bind the 'kernel.debug' parameter value to it
+        #[Autowire(service: 'twig.command.debug')]
+        private DebugCommand $twigDebugCommand
     ){
 
     }
     public function findAll(): array{
+
+        /*
+         *
+         * $output = new BufferedOutput();
+         * $this->twigDebugCommand->tun(new ArrayInput([]), $output);
+         * dd($output);
+         * */
+
         return $this->cache->get('mixes_data', function(CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter(5);
-            $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+            $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/main/mixes.json');
             return $response->toArray();
         });
  }
